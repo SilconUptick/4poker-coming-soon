7e4c5770c48a1ec78855da093814619717386980
Patrick Gerzanics <patrick.gerzanics@upandrunningsoftware.com>
2022-07-29 11:47:29 -0500

    Initial site build.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `4Poker`,
+    description: `4Poker`,
+    author: `@silcon-dev`,
+    siteUrl: `https://4poker.eu`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -19,15 +19,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        formats: [`auto`, `webp`],
+        placeholder: `dominantColor`,
+        quality: 75,
+        name: `4Poker`,
+        short_name: `4Poker`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `transparent`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
